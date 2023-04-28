73aa2de8792fe2b147b8144317c1ee4d0030cbe8
MarwaRMustafa <131968618+MarwaRMustafa@users.noreply.github.com>
2023-04-28 10:09:40 +1000

    Update ViewController.swift
    Test

--- FriGitHubDemo/ViewController.swift
+++ FriGitHubDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //just for testing
-        print("test for changes conflict")
+        print("test for changes test")
         
     }
 
